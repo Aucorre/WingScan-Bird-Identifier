f54d7203f29b92680678ec83bf24036ef910bbbd
ACORRE2 <audren.corre@ynov.com>
2024-04-15 16:46:31 +0200

    imagenerator model

---
+++ model_traingenerator.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+import datetime
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+dataset_path = './download/'
+
+log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2) 
+
+
+train_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(150, 150),
+    batch_size=64,
+    class_mode='categorical', 
+    subset='training') 
+
+
+validation_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(150, 150),
+    batch_size=64,
+    class_mode='categorical',
+    subset='validation')
+
+
+
+
+
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    MaxPooling2D(2, 2),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Flatten(),
+    Dropout(0.2),
+    Dense(512, activation='relu'),
+    Dense(126, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+history = model.fit(
+    train_generator,
+    epochs=10,
+    validation_data=validation_generator)
